5922c1f349f5d6e0bb8f86e0e4635c3a39e244a7
Joanna <joannapawlowicz95@gmail.com>
2023-04-18 14:27:14 +0200

    fix

--- script.js
+++ script.js
@@ -1,62 +1,64 @@
-const colorPicker = new iro.ColorPicker("#color-picker");
-const hexInput = document.getElementById("hex-input");
-const confirmHexButton = document.getElementById("confirm-hex");
-const colorGrid = document.getElementById("color-grid");
-
-function createColorBlock(color) {
-  const colorBlock = document.createElement("div");
-  colorBlock.className = "w-8 h-8 rounded border-2 border-gray-300";
-  colorBlock.style.backgroundColor = color.toHexString();
-  return colorBlock;
-}
-
-function updatePalette() {
-  colorGrid.innerHTML = "";
-  const baseColor = tinycolor(hexInput.value);
-  const palette = generateMaterialPalette(baseColor);
-  for (const color of palette) {
-    const colorBlock = createColorBlock(color);
-    colorGrid.appendChild(colorBlock);
-  }
-}
-
-
-colorPicker.on("color:change", (color) => {
-  hexInput.value = color.hexString;
-  updatePalette();
+const colorPicker = new iro.ColorPicker("#iro-color-picker", {
+  width: 250,
+  color: "#f00",
 });
 
-confirmHexButton.addEventListener("click", updatePalette);
+colorPicker.on("color:change", (color) => {
+  document.getElementById("iro-color-value").value = color.hexString;
+  const palette = generatePalette(color);
+  displayPalette(palette);
+});
 
-hexInput.value = colorPicker.color.hexString;
-updatePalette();
-
-function generateMaterialPalette(baseColor) {
-  const baseLuminance = baseColor.clone().getLuminance();
-  const step = 0.1;
-
-  const shadeLuminanceOffsets = {
-    100: 0.85,
-    200: 0.65,
-    300: 0.45,
-    400: 0.25,
-    500: 0,
-    600: -0.15,
-    700: -0.3,
-    800: -0.45,
-    900: -0.6,
-  };
-
-  const palette = [];
-  for (const shade in shadeLuminanceOffsets) {
-    const targetLuminance = baseLuminance + shadeLuminanceOffsets[shade] * step;
-    const adjustedColor = tinycolor.fromRatio({ l: targetLuminance, s: baseColor.toHsl().s, h: baseColor.toHsl().h });
-    palette.push(adjustedColor);
+function generatePalette(baseColor) {
+  const shades = [];
+  for (let i = 0; i < 9; i++) {
+    const luminance = 0.1 * i;
+    const shade = baseColor.clone().luminance(luminance).hexString;
+    shades.push(shade);
   }
-
-  return palette;
+  return shades;
 }
 
+function displayPalette(palette) {
+  const container = document.getElementById("palette-container");
+  container.innerHTML = "";
+  for (let i = 0; i < palette.length; i++) {
+    const shade = palette[i];
+    const card = document.createElement("div");
+    card.className =
+      "w-32 h-32 bg-white p-2 m-2 rounded shadow flex flex-col justify-between";
+    const colorFill = document.createElement("div");
+    colorFill.style.backgroundColor = shade;
+    colorFill.className = "w-full h-20 rounded";
+    const value = document.createElement("div");
+    value.className = "text-lg font-bold";
+    value.innerText = shade;
+    const description = document.createElement("div");
+    description.className = "text-sm";
+    description.innerText = `Shade ${i + 1}`;
+    card.appendChild(colorFill);
+    card.appendChild(value);
+    card.appendChild(description);
+    container.appendChild(card);
+  }
+}
 
+const initialColor = colorPicker.color;
+const initialPalette = generatePalette(initialColor);
+displayPalette(initialPalette);
 
-    
+const inputField = document.getElementById("iro-color-value");
+inputField.addEventListener("change", (event) => {
+  const hexValue = event.target.value;
+  if (/^#[0-9A-F]{6}$/i.test(hexValue)) {
+    colorPicker.color.hexString = hexValue;
+  }
+});
+
+const submitButton = document.getElementById("submit-color");
+submitButton.addEventListener("click", () => {
+  const hexValue = inputField.value;
+  if (/^#[0-9A-F]{6}$/i.test(hexValue)) {
+    colorPicker.color.hexString = hexValue;
+  }
+});
